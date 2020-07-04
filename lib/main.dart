ddfc2b45cde3bbb0cb5b211301e16733d59f4664
lukmansp <trilukman98@gmail.com>
2020-07-04 08:32:32 +0700

    ard and parsing

--- lib/main.dart
+++ lib/main.dart
@@ -25,50 +25,114 @@ void main() {
 //     ));
 //   }
 // }
+// col&row
+// class HalamanSatu extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.yellow[200],
+//       appBar: new AppBar(
+//         backgroundColor: Colors.red[800],
+//         leading: new Icon(Icons.home),
+//         title: new Center(
+//           child: new Text("lukmansp"),
+//         ),
+//         actions: <Widget>[new Icon(Icons.search)],
+//       ),
+//       body: new Container(
+//         child: new Column(
+//           children: <Widget>[
+//             new Icon(
+//               Icons.local_pizza,
+//               size: 70.0,
+//               color: Colors.red,
+//             ),
+//             new Icon(Icons.cake, size: 70.0, color: Colors.red),
+//             new Row(
+//               children: <Widget>[
+//                 new Icon(
+//                   Icons.donut_large,
+//                   size: 70.0,
+//                   color: Colors.red,
+//                 ),
+//                 new Icon(
+//                   Icons.panorama_fish_eye,
+//                   size: 70.0,
+//                   color: Colors.red,
+//                 ),
+//                 new Icon(
+//                   Icons.donut_large,
+//                   size: 70.0,
+//                   color: Colors.red,
+//                 ),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+// end
 class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[200],
-      appBar: new AppBar(
-        backgroundColor: Colors.red[800],
-        leading: new Icon(Icons.home),
-        title: new Center(
-          child: new Text("lukmansp"),
-        ),
-        actions: <Widget>[new Icon(Icons.search)],
-      ),
+      appBar: new AppBar(title: new Text("card & parsing")),
       body: new Container(
         child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            new Icon(
-              Icons.local_pizza,
-              size: 70.0,
-              color: Colors.red,
+            new CardSaya(
+              icon: Icons.home,
+              teks: "home",
+              warnaicon: Colors.brown,
             ),
-            new Icon(Icons.cake, size: 70.0, color: Colors.red),
-            new Row(
-              children: <Widget>[
-                new Icon(
-                  Icons.donut_large,
-                  size: 70.0,
-                  color: Colors.red,
-                ),
-                new Icon(
-                  Icons.panorama_fish_eye,
-                  size: 70.0,
-                  color: Colors.red,
-                ),
-                new Icon(
-                  Icons.donut_large,
-                  size: 70.0,
-                  color: Colors.red,
-                ),
-              ],
+            new CardSaya(
+              icon: Icons.favorite,
+              teks: "favorite",
+              warnaicon: Colors.pink,
+            ),
+            new CardSaya(
+              icon: Icons.place,
+              teks: "place",
+              warnaicon: Colors.blue,
+            ),
+            new CardSaya(
+              icon: Icons.settings,
+              teks: "setting",
+              warnaicon: Colors.black,
             )
           ],
         ),
       ),
     );
+  }
+}
+
+class CardSaya extends StatelessWidget {
+  CardSaya({this.icon, this.teks, this.warnaicon});
+  final IconData icon;
+  final String teks;
+  final Color warnaicon;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: new EdgeInsets.all(10.0),
+        child: new Card(
+          child: new Column(
+            children: <Widget>[
+              new Icon(
+                icon,
+                size: 50.0,
+                color: warnaicon,
+              ),
+              new Text(
+                teks,
+                style: TextStyle(fontSize: 20.0),
+              )
+            ],
+          ),
+        ));
   }
 }
